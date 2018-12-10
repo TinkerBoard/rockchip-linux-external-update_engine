f0f56a62690baba1cf74bcf060dbf6e6a70dcc6a
jkand.huang <jkand.huang@rock-chips.com>
2018-12-10 15:27:01 +0800

    change url to http://148.70.52.169:8080/update.img
    Change-Id: I01cd8e8f1ea5611776cceec91f2d4643adf06184
    Signed-off-by: jkand.huang <jkand.huang@rock-chips.com>

--- test_kugou.cpp
+++ test_kugou.cpp
@@ -6,7 +6,7 @@ void handle_upgrade_callback(void *user_data, RK_Upgrade_Status_t status){
 }
 
 int main(int argc, char *argv[]){
-    char url[256] = "http://172.16.21.75:8080/update.img";
+    char url[256] = "http://148.70.52.169:8080/update.img";
     RK_ota_set_url(url);
     RK_ota_start(handle_upgrade_callback);
     RK_ota_get_progress();
